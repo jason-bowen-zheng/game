b9fdbc7cf7c576b6c5bbcd72cac18d86fd94c2de
jason-bowen-zheng <13239073174@wo.cn>
2020-10-04 18:22:41 +0800

    2020-10-04

--- gldemo.c
+++ gldemo.c
@@ -3,23 +3,53 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
+int shape = 0;
+
 void init(void) {
 	glEnable(GL_BLEND);
 	glEnable(GL_LINE_SMOOTH);
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 }
 
+void init_menu(void) {
+	glutAddMenuEntry("Triangles", 0);
+	glutAddMenuEntry("Quads", 1);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
+
 void on_draw(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glBegin(GL_TRIANGLES);
-	glColor3f(1.0, 0.0, 0.0);
-	glVertex3f(-0.5, -0.5, 0.0);
-	glColor3f(0.0, 1.0, 0.0);
-	glVertex3f(0.5, 0.0, 0.0);
-	glColor3f(0.0, 0.0, 0.5);
-	glVertex3f(0.0, 0.5, 0.0);
-	glEnd();
+	if (shape == 0) {
+		glBegin(GL_TRIANGLES);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(-0.5, -0.5, 0.0);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.5, 0.0, 0.0);
+		glColor3f(0.0, 0.0, 0.5);
+		glVertex3f(0.0, 0.5, 0.0);
+		glEnd();
+	} else {
+		glBegin(GL_QUADS);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.5, 0.5, 0.0);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(-0.5, 0.5, 0.0);
+		glColor3f(0.0, 0.0, 1.0);
+		glVertex3f(-0.5, -0.5, 0.0);
+		glColor3f(1.0, 0.5, 0.0);
+		glVertex3f(0.5, -0.5, 0.0);
+		glEnd();
+	}
 	glutSwapBuffers();
+}
+
+void on_menu(int value) {
+	if (value == 0) {
+		shape = 0;
+	} else {
+		shape = 1;
+	}
+	on_draw();
 }
 
 void on_reshape(int w, int h) {
@@ -32,6 +62,8 @@ int main(int argc, char *argv[]) {
 	glutInitWindowPosition(-1, -1);
 	glutInitWindowSize(300, 200);
 	glutCreateWindow("Demo");
+	glutCreateMenu(on_menu);
+	init_menu();
 	init();
 	glutDisplayFunc(on_draw);
 	glutReshapeFunc(on_reshape);
