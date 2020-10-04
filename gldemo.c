9bb029b02fe3cb25da605edd36accf6cd87de51f
jason-bowen-zheng <13239073174@wo.cn>
2020-10-04 15:08:06 +0800

    2020-10-04

--- gldemo.c
+++ gldemo.c
@@ -3,32 +3,38 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
-void draw(void) {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glBegin(GL_TRIANGLES);
-	glColor3f(1.0, 0.0, 0.0);
-	glVertex3f(-0.5, -0.5, 0.0);
-	glColor3f(0.0, 1.0, 0.0);
-	glVertex3f(0.5, 0.0, 0.0);
-	glColor3f(0.0, 0.0, 1.0);
-	glVertex3f(0.0, 0.5, 0.0);
-	glEnd();
-	glFlush();
-}
-
 void init(void) {
 	glEnable(GL_BLEND);
 	glEnable(GL_LINE_SMOOTH);
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+}
+
+void on_draw(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glColor3f(1.0, 1.0, 1.0);
+	glutWireTeapot(2.0);
+	glutSwapBuffers();
+}
+
+void on_reshape(int w, int h) {
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60.0, (GLfloat)w / (GLfloat)h, 1.0, 20.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 }
 
 int main(int argc, char *argv[]) {
 	glutInit(&argc, (char **)argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH);
-	glutInitWindowPosition(100, 100);
+	glutInitWindowPosition(-1, -1);
 	glutInitWindowSize(300, 200);
 	glutCreateWindow("Demo");
 	init();
-	glutDisplayFunc(draw);
+	glutDisplayFunc(on_draw);
+	glutReshapeFunc(on_reshape);
 	glutMainLoop();
 	return 0;
 }
